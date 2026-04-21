8a138e1cafa25c1ed7ae8d6d6b0b5f9c49a93b0f
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2026-04-21 22:06:34 +0000

    Implement item change logging in Service Appointment after submission

--- starrain_fsm/field_service_management/doctype/service_appointment/service_appointment.py
+++ starrain_fsm/field_service_management/doctype/service_appointment/service_appointment.py
@@ -24,6 +24,57 @@ class ServiceAppointment(Document):
 			self.validate_overlap()
 		self.update_service_order_status()
 
+	def on_update_after_submit(self):
+		self._log_items_changes()
+
+	def _log_items_changes(self):
+		old_doc = self.get_doc_before_save()
+		if not old_doc:
+			return
+
+		def _to_map(items):
+			result = {}
+			for row in items or []:
+				if not row.item_code:
+					continue
+				if row.item_code in result:
+					result[row.item_code]["qty"] += row.qty
+				else:
+					result[row.item_code] = {
+						"qty": row.qty,
+						"rate": row.rate,
+						"label": row.item_name or row.item_code,
+					}
+			return result
+
+		old_map = _to_map(old_doc.get("items"))
+		new_map = _to_map(self.get("items"))
+		all_codes = sorted(set(old_map) | set(new_map))
+
+		lines = []
+		for code in all_codes:
+			label = (new_map.get(code) or old_map.get(code))["label"]
+			if code not in old_map:
+				lines.append(_("Added <b>{0}</b> &mdash; qty: {1}").format(label, new_map[code]["qty"]))
+			elif code not in new_map:
+				lines.append(_("Removed <b>{0}</b>").format(label))
+			else:
+				old_row, new_row = old_map[code], new_map[code]
+				changes = []
+				if old_row["qty"] != new_row["qty"]:
+					changes.append(_("qty: {0} &rarr; {1}").format(old_row["qty"], new_row["qty"]))
+				if old_row["rate"] != new_row["rate"]:
+					changes.append(_("rate: {0} &rarr; {1}").format(old_row["rate"], new_row["rate"]))
+				if changes:
+					lines.append("<b>{0}</b>: {1}".format(label, ", ".join(changes)))
+
+		if lines:
+			message = "<b>{0}</b><br>{1}".format(
+				_("Items updated"),
+				"<br>".join("&bull; {0}".format(l) for l in lines),
+			)
+			self.add_comment("Edit", message)
+
 	def on_cancel(self):
 		self.cancel_linked_order()
 
