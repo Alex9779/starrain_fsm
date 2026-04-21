9be6dc458677f97ccf1756991030158d210f3efd
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2026-04-21 17:54:39 +0000

    Remove address dynamic link management methods from ServiceRequest class

--- starrain_fsm/field_service_management/doctype/service_request/service_request.py
+++ starrain_fsm/field_service_management/doctype/service_request/service_request.py
@@ -7,61 +7,7 @@ from frappe.utils import add_days, getdate, today
 
 
 class ServiceRequest(Document):
-	def after_insert(self):
-		self._sync_address_dynamic_link(old_address=None)
-
-	def on_update(self):
-		old_address = self.get_doc_before_save() and self.get_doc_before_save().customer_address
-		self._sync_address_dynamic_link(old_address=old_address)
-
-	def on_trash(self):
-		self._remove_address_dynamic_link(self.customer_address)
-
-	def _sync_address_dynamic_link(self, old_address):
-		new_address = self.customer_address
-		if old_address == new_address:
-			return
-		if old_address:
-			self._remove_address_dynamic_link(old_address)
-		if new_address:
-			self._add_address_dynamic_link(new_address)
-
-	def _add_address_dynamic_link(self, address_name):
-		already_linked = frappe.db.exists(
-			"Dynamic Link",
-			{
-				"parenttype": "Address",
-				"parent": address_name,
-				"link_doctype": self.doctype,
-				"link_name": self.name,
-			},
-		)
-		if not already_linked:
-			link = frappe.new_doc("Dynamic Link")
-			link.update(
-				{
-					"parenttype": "Address",
-					"parent": address_name,
-					"parentfield": "links",
-					"link_doctype": self.doctype,
-					"link_name": self.name,
-					"link_title": self.name,
-				}
-			)
-			link.insert(ignore_permissions=True)
-
-	def _remove_address_dynamic_link(self, address_name):
-		if not address_name:
-			return
-		frappe.db.delete(
-			"Dynamic Link",
-			{
-				"parenttype": "Address",
-				"parent": address_name,
-				"link_doctype": self.doctype,
-				"link_name": self.name,
-			},
-		)
+	pass
 
 
 def update_status():
